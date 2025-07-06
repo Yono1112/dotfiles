73dfd352d2a4f384fdb7dd03545fa5dc5f34be7e
Yono1112 <yuma.ohno.sss@gmail.com>
2025-07-06 22:08:22 +0900

    add custom key mappings in nvim-tree

--- nvim/lua/plugins/file-explorer.lua
+++ nvim/lua/plugins/file-explorer.lua
@@ -10,6 +10,32 @@ return {
         enable = true,
         update_root = true,
       },
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+
+	-- custom key mapping
+        vim.keymap.del("n", "e", { buffer = bufnr })
+        vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory / Parent'))
+
+        vim.keymap.set('n', 'H', api.tree.change_root_to_parent, opts('Change Root Up'))
+        vim.keymap.set('n', 'l', api.node.open.edit, opts('Open File / Expand Folder'))
+        vim.keymap.set('n', 'R', api.tree.change_root_to_node, opts('Change Root to Node'))
+        vim.keymap.set('n', '<Space>', function()
+          local node = api.tree.get_node_under_cursor()
+          if not node then return end
+
+          if node.type == "directory" then
+            api.node.open.edit()
+          else
+            api.node.open.preview()
+          end
+        end, opts('Toggle Preview / Expand Folder'))
+      end,
     },
   },
 }
