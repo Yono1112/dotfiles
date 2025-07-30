3dce0ea6f2a3c91ca20161131755dc22c172642b
Yono1112 <yuma.ohno.sss@gmail.com>
2025-07-31 01:08:58 +0900

    add transparent config to Neovim as well

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,29 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "moon",
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
-      vim.api.nvim_set_hl(0, "WinSeparator", {
-        fg = "#3b4261",
-      })
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "moon",
+			transparent = true,
+			-- nvim-treeの背景色を透明に
+			on_highlights = function(hl, c)
+				hl.NvimTreeNormal = {
+					bg = "none",
+					fg = c.fg_sidebar,
+				}
+				hl.NvimTreeNormalNC = {
+					bg = "none",
+					fg = c.fg_sidebar,
+				}
+			end,
+		},
+		config = function(_, opts)
+			require("tokyonight").setup(opts)
+			vim.cmd.colorscheme("tokyonight")
+			vim.api.nvim_set_hl(0, "WinSeparator", {
+				fg = "#3b4261",
+			})
+		end,
+	},
 }
