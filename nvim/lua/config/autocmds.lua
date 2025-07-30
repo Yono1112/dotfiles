9e4eff834712340c68eb2107d01b03cd6ad008db
Yono1112 <yuma.ohno.sss@gmail.com>
2025-07-30 18:04:54 +0900

    add comment plugins in neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,20 +3,23 @@ local api = vim.api
 local ts_group = api.nvim_create_augroup("MyTreesitter", { clear = true })
 
 local target_languages = {
-  "lua",
-  "typescript",
-  "rust",
-  "markdown",
-  "terraform",
-  "python",
+	"lua",
+	"typescript",
+	"rust",
+	"markdown",
+	"terraform",
+	"python",
+	"tsx",
+	"javascript",
+	"html",
 }
 
 api.nvim_create_autocmd("FileType", {
-  group = ts_group,
-  callback = function()
-    if vim.tbl_contains(target_languages, vim.bo.filetype) then
-      vim.treesitter.start()
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end
-  end,
+	group = ts_group,
+	callback = function()
+		if vim.tbl_contains(target_languages, vim.bo.filetype) then
+			vim.treesitter.start()
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
+	end,
 })
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,43 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
-  lazy = false,
-  config = function()
-    require("nvim-treesitter").install({
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "typescript",
-      "rust",
-      "markdown",
-      "markdown_inline",
-      "terraform",
-      "python",
-      "yaml",
-      "toml",
-      "cpp",
-      "bash",
-      "javascript",
-    })
-    -- }):wait()
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").install({
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"typescript",
+			"typescriptreact",
+			"rust",
+			"markdown",
+			"markdown_inline",
+			"terraform",
+			"python",
+			"yaml",
+			"toml",
+			"cpp",
+			"bash",
+			"javascript",
+			"javascriptreact",
+			"tsx",
+		})
+		-- }):wait()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"typescript",
+				"typescriptreact",
+				"tsx",
+				"javascript",
+				"html",
+			},
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.commentstring = "// %s"
+			end,
+		})
+	end,
 }
