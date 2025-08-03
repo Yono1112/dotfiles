4a6ada183aabac4783bf84ec333016afc7702756
Yono1112 <yuma.ohno.sss@gmail.com>
2025-08-04 03:33:23 +0900

    change formatter timeout

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("conform").setup({
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 1000,
 				lsp_fallback = true,
 			},
 			formatters_by_ft = {
